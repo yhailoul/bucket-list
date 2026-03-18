86d8de7e82959cab9fc45916b6af2c422bfa6043
yhailoul <yasmine.hailoul2025@campus-eni.fr>
2026-03-18 10:37:31 +0100

    création de user fixtures lors de la création d'un wish

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,15 +4,20 @@ namespace App\DataFixtures;
 
 use App\Entity\Category;
 use App\Entity\Wish;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher){
+
+    }
     public function load(ObjectManager $manager): void
     {
-
         $faker = Factory::create('fr_FR');
 
         $categoryNames = [
@@ -32,6 +37,19 @@ class AppFixtures extends Fixture
             $categories[] = $category;
         }
 
+        $users=[];
+        for($i = 0; $i < 50; $i++) {
+            $user = new User();
+            $user
+                ->setRoles(['ROLE_USER'])
+                ->setEmail($faker->email())
+                ->setPseudo($faker->userName())
+                ->setPassword(
+                    $this->userPasswordHasher->hashPassword($user, '123456'));
+            $manager->persist($category);
+            $users[] = $user;
+        }
+
         $manager->flush();
 
         for ($i = 0; $i < 50; $i++) {
@@ -40,7 +58,7 @@ class AppFixtures extends Fixture
                 ->setTitle($faker->sentence(6,[true]))
                 ->setDateCreated($faker->dateTimeBetween('-5 years','now'))
                 ->setDateUpdated($faker->dateTimeBetween('-5 years','now'))
-                ->setAuthor($faker->userName())
+                ->setUser($faker->randomElement($users))
                 ->setDescription($faker->text(255))
                 ->setIsPublished(true)
                 ->setCategory($faker->randomElement($categories));
